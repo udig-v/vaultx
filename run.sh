6a370b7652c54ca8dfdafeb6e9e381e4ea95c008
Varvara <vbondarenko@hawk.iit.edu>
2024-11-17 20:06:20 +0000

    Added script file

---
+++ run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+for K in {25..35}
+do
+    ./vaultx_86 -a for -t 64 -K $K -m 16384 -b 8192 -f vaultx$K.memo.tmp -g vaultx$K.memo
+done 
+
+for hash_size in 3 4 5 6 7 8 16 32
+do
+    ./vaultx_86 -t 64 -L 1000,$hash_size -g vaultx$K.memo
+done
