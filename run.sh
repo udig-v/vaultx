f26590a6934cf0bfe02d1d105cf38bacadd39b38
Varvara <vbondarenko@hawk.iit.edu>
2024-11-26 16:57:42 +0000

    Changed benchmarking script

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ make clean
 make vaultx_x86 NONCE_SIZE=4
 for K in {25..31}
 do
-	./vaultx_x86 -a for -t 64 -K $K -m 16384 -b 8192 -f vaultx$K.memo.tmp -g vaultx$K.memo
+	./vaultx_x86 -a for -t 64 -K $K -m 16384 -b 8192 -f /data-fast/varvara/vaultx$K.memo.tmp -g /data-l/varvara/vaultx$K.memo
 	for hash_size in 3 4 5 6 7 8 16 32
 	do
     		./vaultx_x86 -t 64 -L 1000,$hash_size -g vaultx$K.memo
@@ -15,7 +15,7 @@ make clean
 make vaultx_x86 NONCE_SIZE=5
 for K in {32..35}
 do
-	./vaultx_x86 -a for -t 64 -K $K -m 16384 -b 8192 -f vaultx$K.memo.tmp -g vaultx$K.memo
+	./vaultx_x86 -a for -t 64 -K $K -m 16384 -b 8192 -f /data-fast/varvara/vaultx$K.memo.tmp -g /data-l/varvara/vaultx$K.memo
 	for hash_size in 3 4 5 6 7 8 16 32
 	do
     		./vaultx_x86 -t 64 -L 1000,$hash_size -g vaultx$K.memo
