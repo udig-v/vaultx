40254df85e2f0b74ed54cb5a3c772afb9a7e7075
udig-v <vbondarenko@hawk.iit.edu>
2024-12-05 03:43:15 +0800

    Added lookup data

--- run.sh
+++ run.sh
@@ -4,11 +4,11 @@ make clean
 make vaultx_arm NONCE_SIZE=4
 for K in {25..31}
 do
-	./vaultx -a for -t 64 -K $K -m 16384 -b 8192 -f vaultx$K.memo.tmp -g vaultx$K.memo
+	./vaultx -a for -t 8 -K $K -m 16384 -b 8192 -f vaultx$K.memo.tmp -g vaultx$K.memo
 	for hash_size in 3 4 5 6 7 8 16 32
 	do
 		./drop-all-caches.sh
-    		./vaultx -t 64 -L 1000,$hash_size -g vaultx$K.memo
+    		./vaultx -t 8 -L 1000,$hash_size -g vaultx$K.memo
 	done
 done
 
@@ -16,10 +16,10 @@ make clean
 make vaultx_arm NONCE_SIZE=5
 for K in {32..35}
 do
-	./vaultx -a for -t 64 -K $K -m 16384 -b 8192 -f vaultx$K.memo.tmp -g vaultx$K.memo
+	./vaultx -a for -t 8 -K $K -m 16384 -b 8192 -f vaultx$K.memo.tmp -g vaultx$K.memo
 	for hash_size in 3 4 5 6 7 8 16 32
 	do
-    		./drop-all-caches
-		./vaultx -t 64 -L 1000,$hash_size -g vaultx$K.memo
+    		./drop-all-caches.sh
+		./vaultx -t 8 -L 1000,$hash_size -g vaultx$K.memo
 	done
 done
