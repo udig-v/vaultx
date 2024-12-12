7811cb6603b4e70014d272feb5b3b33d7c313049
Varvara <vbondarenko@hawk.iit.edu>
2024-12-12 17:05:45 +0000

    Changed time precision

--- vaultx.c
+++ vaultx.c
@@ -816,7 +816,7 @@ void search_memo_records_batch(const char *filename, int num_lookups, int search
     // else
     //	printf("no NONCE found for HASH prefix %s\n",SEARCH_STRING);
     if (!BENCHMARK)
-        printf("searched for %d lookups of %d bytes long, found %d, not found %d in %.2f seconds, %.2f ms per lookup\n", num_lookups, search_size, foundRecords, notFoundRecords, elapsed_time / 1000.0, elapsed_time / num_lookups);
+        printf("searched for %d lookups of %d bytes long, found %d, not found %d in %.2f seconds, %.4f ms per lookup\n", num_lookups, search_size, foundRecords, notFoundRecords, elapsed_time / 1000.0, elapsed_time / num_lookups);
     else
         printf("%s %d %zu %llu %llu %d %d %d %d %.2f %.2f\n", filename, NUM_THREADS, filesize, num_buckets_search, num_records_in_bucket_search, num_lookups, search_size, foundRecords, notFoundRecords, elapsed_time / 1000.0, elapsed_time / num_lookups);
     // return NULL;
